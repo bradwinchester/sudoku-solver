d0ab25497f88ef7dc758dbbad10462d856c674da
bradwinchester <104231576+bradwinchester@users.noreply.github.com>
2024-06-13 11:41:25 -0600

    added function to identify hidden pairs

--- Solver.cpp
+++ Solver.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <set>
+#include <string>
 
 // constructor
 Solver::Solver(Puzzle p) 
@@ -18,8 +19,11 @@ void Solver::solve()
 	while (!checkIfSolved()) {
 		updateNotes();
 		updateNakedPairs();
-		updateNakedTriples();
-
+		updateNakedTriples("row");
+		updateNakedTriples("col");
+		updateNakedTriples("box");
+		updateHiddenPairs();
+		
 		count++;
 		if (count > 100) { break; } // prevents infinite loop
 	}
@@ -183,16 +187,18 @@ void Solver::removeNakedPair(int p1, int p2, std::vector<int> pair, std::vector<
 }
 
 // searches for matching triplets, and removes those possibilites from the other cells in it's module
-void Solver::updateNakedTriples()
+void Solver::updateNakedTriples(std::string mod_type)
 {
 	for (int i = 1; i <= 81; i++) {
 		std::vector c1 = puzzle.getCell(i);
 		if (c1.size() > 3 or c1.size() == 1) { continue; } // skip if there are more than three possibilities or the cell is solved
 		
-		// remove triplets from rows
+		std::vector<int> module{};
+		if (mod_type == "row") { module = puzzle.getRowIds(i); }
+		if (mod_type == "col") { module = puzzle.getColIds(i); }
+		if (mod_type == "box") { module = puzzle.getBoxIds(i); }
 		
-		std::vector<int> row = puzzle.getRowIds(i);
-		for (int j : row) {
+		for (int j : module) {
 			if (i == j) { continue; }
 
 			std::set<int> triplet{};
@@ -204,7 +210,7 @@ void Solver::updateNakedTriples()
 			for (auto x : c2) { triplet.insert(x); }
 			if (triplet.size() > 3) { continue; } // not triplets, break
 
-			for (int k : row) {
+			for (int k : module) {
 				if (i == k or j == k) { continue; }
 				std::vector c3 = puzzle.getCell(k);
 				if (c3.size() > 3 or c3.size() == 1) { continue; }
@@ -212,59 +218,7 @@ void Solver::updateNakedTriples()
 				for (auto x : c3) { triplet.insert(x); }
 				if (triplet.size() > 3) { continue; } // not triplets, break
 				
-				if (triplet.size() == 3) { removeNakedTriplets(i, j, k, triplet, row); }
-			}
-		}
-
-		// remove triplets from cols
-		std::vector<int> col = puzzle.getColIds(i);
-		for (int j : col) {
-			if (i == j) { continue; }
-
-			std::set<int> triplet{};
-			for (auto x : c1) { triplet.insert(x); } // add all elements of first cell to a set
-
-			std::vector c2 = puzzle.getCell(j);
-			if (c2.size() > 3 or c2.size() == 1) { continue; }
-
-			for (auto x : c2) { triplet.insert(x); }
-			if (triplet.size() > 3) { continue; } // not triplets, break
-
-			for (int k : col) {
-				if (i == k or j == k) { continue; }
-				std::vector c3 = puzzle.getCell(k);
-				if (c3.size() > 3 or c3.size() == 1) { continue; }
-
-				for (auto x : c3) { triplet.insert(x); }
-				if (triplet.size() > 3) { continue; } // not triplets, break
-
-				if (triplet.size() == 3) { removeNakedTriplets(i, j, k, triplet, col); }
-			}
-		}
-
-		// remove triplets from boxes
-		std::vector<int> box = puzzle.getBoxIds(i);
-		for (int j : box) {
-			if (i == j) { continue; }
-
-			std::set<int> triplet{};
-			for (auto x : c1) { triplet.insert(x); } // add all elements of first cell to a set
-
-			std::vector c2 = puzzle.getCell(j);
-			if (c2.size() > 3 or c2.size() == 1) { continue; }
-
-			for (auto x : c2) { triplet.insert(x); }
-			if (triplet.size() > 3) { continue; } // not triplets, break
-
-			for (int k : box) {
-				if (i == k or j == k) { continue; }
-				std::vector c3 = puzzle.getCell(k);
-				if (c3.size() > 3 or c3.size() == 1) { continue; }
-
-				for (auto x : c3) { triplet.insert(x); }
-				if (triplet.size() > 3) { continue; } // not triplets, break
-
-				if (triplet.size() == 3) { removeNakedTriplets(i, j, k, triplet, box); }
+				if (triplet.size() == 3) { removeNakedTriplets(i, j, k, triplet, module); }
 			}
 		}
 	}
@@ -283,6 +237,52 @@ void Solver::removeNakedTriplets(int i, int j, int k, std::set<int> triplet, std
 		}
 	}
 }
+
+// iterates through every row, column, and box (module) to find hidden pairs 
+void Solver::updateHiddenPairs()
+{
+	for (int i = 1; i <= 73; i += 9) { isolateHiddenPairs(i, "row"); }
+	for (int i = 1; i <= 9 ; i++) { isolateHiddenPairs(i, "col"); }
+	
+	std::vector<int> box_ids = { 1, 4, 7, 28, 31, 34, 55, 58, 61 };
+	for (auto i : box_ids) {
+		isolateHiddenPairs(i, "box");
+	}
+}
+
+// removes all other possibilites from cells containing hidden pairs 
+void Solver::isolateHiddenPairs(int index, std::string mod_type)
+{
+	std::vector<int> module{};
+	if (mod_type == "row") { module = puzzle.getRowIds(index); }
+	if (mod_type == "col") { module = puzzle.getColIds(index); }
+	if (mod_type == "box") { module = puzzle.getBoxIds(index); }
+
+	std::map<int, std::vector<int>> found_idx{};
+	for (int j = 1; j <= 9; j++) { // each possible number in a cell
+		for (int k : module) { // each cell in module
+			std::vector<int> cell = puzzle.getCell(k);
+			if (cell.size() > 1) {
+				if (std::find(cell.begin(), cell.end(), j) != cell.end()) {
+					found_idx[j].push_back(k);
+				}
+			}
+		}
+	}
+	std::map<int, std::vector<int>> pairs{};
+	for (auto const& [key, val] : found_idx) {
+		for (int key2 = 1; key2 <= 9; key2++) {
+			if (key == key2) { continue; }
+			std::vector<int> val2 = found_idx[key2];
+			if (val == val2 and val.size() == 2) {
+				puzzle.updateCell(val[0], { key, key2 });
+				puzzle.updateCell(val[1], { key, key2 });
+			}
+		}
+	}
+}
+
+
 
 
 
--- Solver.h
+++ Solver.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Puzzle.h"
 #include <set>
+#include <string>
 
 class Solver : public Puzzle
 {
@@ -13,8 +14,11 @@ public:
 	void removeNumberFromModule(int cell_id, int value);
 	void updateNakedPairs();
 	void removeNakedPair(int p1, int p2, std::vector<int> pair, std::vector<int> module);
-	void updateNakedTriples();
+	void updateNakedTriples(std::string mod_type);
 	void removeNakedTriplets(int i, int j, int k, std::set<int> s, std::vector<int> module);
+	void updateHiddenPairs();
+	void isolateHiddenPairs(int index, std::string mod_type);
+	
 
 private:
 	Puzzle puzzle{};
